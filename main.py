836697939baa00618df2670507f8d6773daadfdc
StudyingDragon <amanda.e.farmer@gmail.com>
2026-06-30 17:21:30 -0400

    Changed user prompt from single to list

--- main.py
+++ main.py
@@ -3,22 +3,30 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main() -> None:
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    args = parser.parse_args()
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
         raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
-    parser = argparse.ArgumentParser(description="Chatbot")
-    parser.add_argument("user_prompt", type=str, help="User prompt")
-    args = parser.parse_args()
-
     client = genai.Client(api_key=api_key)
+    messages: list[types.Content] = [
+        types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
+    ]
+    generate_content(client, messages)
+
+
+def generate_content(client: genai.Client, messages: list[types.Content]) -> None:
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents=args.user_prompt,
+        contents=messages,
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
