70281d42d23edca5dcbe55103d6c4cd29b0a32b1
StudyingDragon <amanda.e.farmer@gmail.com>
2026-06-30 17:48:11 -0400

    Added verbose option to prompt

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from google.genai import types
 def main() -> None:
     parser = argparse.ArgumentParser(description="AI Code Assistant")
     parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     load_dotenv()
@@ -20,10 +21,15 @@ def main() -> None:
     messages: list[types.Content] = [
         types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
     ]
-    generate_content(client, messages)
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+
+    generate_content(client, messages, args.verbose)
 
 
-def generate_content(client: genai.Client, messages: list[types.Content]) -> None:
+def generate_content(
+    client: genai.Client, messages: list[types.Content], verbose: bool
+) -> None:
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
@@ -31,8 +37,9 @@ def generate_content(client: genai.Client, messages: list[types.Content]) -> Non
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
 
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
 
